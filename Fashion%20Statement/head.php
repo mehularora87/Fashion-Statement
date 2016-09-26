9e87b3862085c6197d548a352b93198d44fb3907
Mehul Arora <mehularora87@gmail.com>
2016-09-26 23:07:09 +1300

    Add files via upload

---
+++ Fashion%20Statement/head.php
@@ -0,0 +1,13 @@
+<html>
+<!--This page is like the navigation bar for moving between different pages-->
+
+<ul id="link">
+	<li><a href="welcomepage.php">Home</a></li>					<!--Home Page-->
+	<li><a href="privacy_policy.html">Sign up</a></li>				<!--Sign Up Page-->
+	<li><a href="login.php">Login</a></li>						<!--Login-->
+	<li><a href="ProfileUpdate.php">Profile</a></li>
+	<li><a href="about.php">About</a></li> 					<!--About Page-->
+	
+</ul> 
+
+</html>
