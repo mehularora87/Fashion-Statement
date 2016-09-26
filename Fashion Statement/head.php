55504fa2773027bd797d54b3fc60b45501df3949
Mehul Arora <mehularora87@gmail.com>
2016-09-26 23:09:40 +1300

    Update head.php

--- Fashion Statement/head.php
+++ Fashion Statement/head.php
@@ -3,7 +3,7 @@
 
 <ul id="link">
 	<li><a href="welcomepage.php">Home</a></li>					<!--Home Page-->
-	<li><a href="signUp.php">Sign up</a></li>				<!--Sign Up Page-->
+	<li><a href="privacy_policy.html">Sign up</a></li>				<!--Sign Up Page-->
 	<li><a href="login.php">Login</a></li>						<!--Login-->
 	<li><a href="ProfileUpdate.php">Profile</a></li>
 	<li><a href="about.php">About</a></li> 					<!--About Page-->
