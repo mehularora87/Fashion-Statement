3e53634afa1402ca5b93603731b9f123b21c0d9b
Mehul Arora <mehularora87@gmail.com>
2016-10-07 14:30:53 +1300

    Update connection.php

--- Fashion Statement/connection.php
+++ Fashion Statement/connection.php
@@ -1,20 +1,25 @@
 
 <?php
-	$user = $_POST["txtUsername"];
+
+	<!--getting data from the sign up page through post message-->
+		
+	$user = $_POST["txtUsername"];			
 	$password = md5($_POST["pwdPass1"]);
 	$password2 = md5($_POST["pwdPass2"]);
 	$age = $_POST["age"];
 	$email = $_POST["email"];
 	$gender = $_POST["rdoGender"];
 	
-	//need database info
+	<!--connecting to the server-->
 	$connect = mysqli_connect("localhost", "root", "","fashion") or die ("Unable to connect to MySQL database");
+	
+	<!--connecting to the database-->
 	$connectdb = mysqli_select_db($connect, "fashion");
 		$queryAdd = "INSERT INTO accounts(username, password, email, gender, age)
 						VALUES('$user', '$password', '$email', '$gender', '$age')";
-		mysqli_query($connect,$queryAdd);
+		mysqli_query($connect,$queryAdd);	<!--running the query-->
 		
-		mysqli_close($connect);	
+		mysqli_close($connect);			<!--disconnecting from the server-->
 		
 	
 	
