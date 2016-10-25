594a52a759df5d2c33b92bbce066af79d1fc7db8
JamesATCollie <jamescollie@gmail.com>
2016-10-25 11:40:08 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset = "utf-8" />
+		<link href="style.css" rel="stylesheet">
+		<title>Welcome Page</title>
+	</head>
+	<?php include "head.php" ?><br/><br/><br/><br/><br/>
+	<hr/>
+	
+	<BODY BGCOLOR="WHITE">
+<CENTER>
+<H1>FASHION STATEMENT - About</H1>
+
+  <H4>Group 1 - James Collie (Product Owner & Developer), Mehul Arora (SCRUM Master & Developer), Sherry Wu (Developer) & Jeremy He (Developer)</H4>
+
+<H2>A website about the searching and browsing of clothing items.</H2>
+
+</CENTER>
+
+<H3>What is FASHION STATEMENT?</H3>
+
+This is a project for the Auckland University of Technology's Software Development Practice paper.<P>
+
+The following website was developed under the SCRUM development framework.
+
+The team hopes you enjoy making use of our site!
+
+<H3>What does FASHION STATEMENT do?</H3>
+
+FASHION STATEMENT allows users to browse and 'like' clothing items within the web site database.
+
+The team hopes that FASHION STATEMENT will allow all users to broaden their perspective on modern fashion and share their tastes with other users!
+ 
+
+Modifiers are sometimes included along with the basic command, inside the opening command's < >. The modifier does not need to be repeated in the closing command.
+
+</body>
+
+
+
+</html>
